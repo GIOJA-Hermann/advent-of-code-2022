ffb69bca758c41fed1f6e6cf2f3634549d83bc05
Woxhound <giojahermann@gmail.com>
2022-12-12 22:09:45 +0100

    add day10 and day11

---
+++ day10.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void day10(char *part){
+    void calc(int *C, int *X, int *tot) {
+        if (!((++(*C)-20)%40))
+            *tot += (*C)*(*X);
+    }
+    void docalc() {
+        char c[5];
+        int p;
+        int C=1, X=1, tot = 0;
+        while(scanf("%s %d\n", c, &p) >= 1) {
+            if(strcmp(c, "noop") == 0)
+                calc(&C,&X,&tot);
+            else {
+                calc(&C,&X,&tot);
+                X += p;
+                calc(&C,&X,&tot);
+            }
+        }
+        printf("%d", tot);
+    }
+    
+    void prerender(int *C, char *S, int m, int M) {
+        S[*C] = (((*C)%40) >= m && ((*C)%40) <= M)?'#':'.';
+        (*C)++;
+    }
+
+    void dorender() {
+        char c[5];
+        int p;
+        char S[240] = {0};
+        int C = 0;
+        int m = 0, M = 2;
+        prerender(&C,S,m,M);
+        while(scanf("%s %d\n", c, &p) >= 1) {
+            if(strcmp(c, "noop") == 0)
+                prerender(&C,S,m,M);
+            else {
+                prerender(&C,S,m,M);
+                m += p;
+                M += p;
+                prerender(&C,S,m,M);
+            }
+        }
+        for(char *P = S; P < (S+240); P += 40)
+            printf("%.40s\n", P);
+    }
+    
+    if (!strcmp(part, "p1")) {
+        docalc();
+    } else {
+        dorender();
+    }
+}
+
+int main() {
+    day10("p1");
+}
---
+++ day11.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//#define DEBUG1
+//#define DEBUG2
+
+#ifdef DEBUG1
+    #define PRINTF1(args...) printf(args)
+#else
+    #define PRINTF1(args...) {}
+#endif
+#ifdef DEBUG2
+    #define PRINTF2(args...) printf(args)
+#else
+    #define PRINTF2(args...) {}
+#endif
+
+void day11(char *part) {
+    struct worry_s;
+    //typedef worry_s worry_t;
+    
+    typedef struct worry_s {
+        long long val;
+        struct worry_s *wor;
+    } worry_t;
+    
+    typedef struct {
+        worry_t *wors;
+        worry_t *ewor;
+        long long (*op)(long long, long long);
+        int usefixop;
+        int fixop;
+        int divtest;
+        int gotrue;
+        int gofalse;
+        int count;
+    } monkey_t;
+    
+    long long adder(long long a, long long b) {
+        return a+b;
+    }
+    long long multiplier(long long a, long long b) {
+        return a*b;
+    }
+
+    monkey_t monkeys[10] = {0};
+    worry_t wors[40] = {0};
+    int nwors = 0;
+    char s[100], op[2], fixop[5];
+    char *seps = " :,";
+    int idx;
+    char *tok;
+    int globalmod = 1;
+    
+    while (scanf("%*s %d:\n", &idx) == 1) {
+        PRINTF1("LINE 1 --- IDX: %d\n", idx);
+        fgets(s, 100, stdin);
+        tok = strtok(s, seps); // Starting
+        tok = strtok(NULL, seps); //items
+        while((tok = strtok(NULL, seps)) != NULL) {
+            if (strcmp(tok, "0\n")) {
+                if (monkeys[idx].ewor == NULL) {
+                    PRINTF1("LINE 2 --- Add first ");
+                    monkeys[idx].wors = &wors[nwors++];
+                    monkeys[idx].ewor = monkeys[idx].wors;
+                } else {
+                    PRINTF1("LINE 2 --- Add other ");
+                    monkeys[idx].ewor->wor = &wors[nwors++];
+                    monkeys[idx].ewor = monkeys[idx].ewor->wor;
+                }
+                PRINTF1(": %d\n", atoi(tok));
+                monkeys[idx].ewor->val = atoi(tok);
+                monkeys[idx].ewor->wor = NULL;
+            }
+        }
+        scanf("  %*s %*s %*s %*s %s %s", op, fixop);
+        if (op[0] == '+') {
+            PRINTF1("LINE 3 --- ADDER ");
+            monkeys[idx].op = &adder;
+        } else {
+            PRINTF1("LINE 3 --- MULTIPLIER ");
+            monkeys[idx].op = &multiplier;
+        }
+        if (!strcmp(fixop, "old")) {
+            monkeys[idx].usefixop = 0;
+            PRINTF1("(old, old)\n");
+        } else {
+            monkeys[idx].usefixop = 1;
+            monkeys[idx].fixop = atoi(fixop);
+            PRINTF1("(old, %d)\n", monkeys[idx].fixop);
+        }
+        scanf("  %*s %*s %*s %d", &monkeys[idx].divtest);
+        globalmod = globalmod * monkeys[idx].divtest;
+        PRINTF1("LINE 4 --- DIV: %d\n", monkeys[idx].divtest);
+        scanf("    %*s %*s %*s %*s %*s %d", &monkeys[idx].gotrue);
+        PRINTF1("LINE 5 --- TRUE: %d\n", monkeys[idx].gotrue);
+        scanf("    %*s %*s %*s %*s %*s %d", &monkeys[idx].gofalse);
+        PRINTF1("LINE 6 --- FALSE: %d\n", monkeys[idx].gofalse);
+        //fgets(s, 100, stdin);
+        PRINTF1("\n");
+    }
+    
+    PRINTF2("globalmod = %d\n", globalmod);
+    
+    for (int i=0; i<((strcmp(part, "p1"))?10000:20); i++) {
+        PRINTF1("ROUND %d\n", i);
+        for (int j=0; j <= idx; j++) {
+            worry_t *nwor, *wor = monkeys[j].wors;
+            while (wor != NULL) {
+                PRINTF1("MONKEY %d: worry %lld => ", j, wor->val);
+                monkeys[j].wors = wor->wor;
+                monkeys[j].count++;
+                if(monkeys[j].usefixop) {
+                    wor->val = monkeys[j].op(monkeys[j].fixop, wor->val);
+                } else {
+                    wor->val = monkeys[j].op(wor->val, wor->val);
+                }
+                if (strcmp(part, "p1"))
+                    wor->val = wor->val % globalmod;
+                else
+                    wor->val = wor->val /3;
+
+                int nidx = (wor->val % monkeys[j].divtest)?monkeys[j].gofalse:monkeys[j].gotrue;
+                PRINTF1("%lld to MONKEY %d\n", wor->val, nidx);
+                if (monkeys[nidx].ewor == NULL) {
+                    monkeys[nidx].wors = wor;
+                } else {
+                    monkeys[nidx].ewor->wor = wor;
+                }
+                monkeys[nidx].ewor = wor;
+                nwor = wor->wor;
+                wor->wor = NULL;
+                wor = nwor;
+            }
+            monkeys[j].ewor = NULL;
+        }
+    }
+    long long max[2] = {0};
+    for (int j=0; j <= idx; j++) {
+        PRINTF2("Monkey %d inspected items %d times.\n", j, monkeys[j].count);
+        if(monkeys[j].count > max[0]) {
+            if(monkeys[j].count > max[1]) {
+                max[0] = max[1];
+                max[1] = monkeys[j].count; 
+            } else {
+                max[0] = monkeys[j].count;
+            }
+        }
+    }
+    printf("%lld\n", max[0]*max[1]);
+}
+
+int main() {
+    day11("p2");
+}
